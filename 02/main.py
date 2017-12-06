ab369fb1745d232c55cf8601bcbd8745ca90ec82
Thiago Bohn <thiago.bohn@sap.com>
2017-12-06 13:56:55 -0200

    Day 2 - Second Part

--- 02/main.py
+++ 02/main.py
@@ -1,9 +1,27 @@
 data = list(open("02\input.txt").readlines())
 data = map(lambda r: map(lambda c: int(c), r.split('\t')), data)
 
-differences = []
+def first_part(data):
 
-for line in data:
-    differences.append(max(line) - min(line))
+    differences = []
 
-print sum(differences)
+    for line in data:
+        differences.append(max(line) - min(line))
+
+    return sum(differences)
+
+def second_part(data):
+
+    divisors = []
+
+    for line in data:
+        for col in line:
+            divisor = filter(lambda n: col != n and col % n == 0, line)
+            
+            if len(divisor) > 0:
+                divisors.append(col / divisor[0])
+
+    return sum(divisors)
+
+print first_part(data)
+print second_part(data)
