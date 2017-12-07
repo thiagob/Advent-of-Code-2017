d33baa141608720c629ff55c05b2e91decbbf305
Thiago Bohn <thiago.bohn@sap.com>
2017-12-06 23:20:31 -0200

    Day 3 - Second Star

--- 03/main.py
+++ 03/main.py
@@ -9,14 +9,64 @@ class Direction(Enum):
 
 class Spiral:
 
-    def __init__(self):
+    def __init__(self, size):
+        self.size = size
+        self.init_matrix()
+
+    def init_matrix(self):
         self.current = 0
-        self.matrix = np.zeros(shape=(1000, 1000))
-        self.x, self.y = starting_point(self.matrix)
+        self.matrix = np.zeros(shape=(self.size, self.size))
+        self.x, self.y = self.starting_point()
+
+    def starting_point(self):
+        center = len(self.matrix) / 2
+        return center, center
     
-    def assign(self):
-        self.current += 1
-        self.matrix[self.x, self.y] = self.current
+    def fill(self, input):
+        self.init_matrix()
+
+        movement = Direction.DOWN
+        while self.current < input:
+            movement = self.next_direction(movement)
+            self.current += 1
+            self.assign(self.current)
+            self.move(movement)
+
+    def stress_test(self, input):
+        self.init_matrix()
+        
+        adjs = 0
+        movement = Direction.DOWN
+
+        while True:
+            movement = self.next_direction(movement)
+            adjs = self.adjacents()
+            self.assign(adjs)
+
+            if adjs > input:
+                break;            
+
+            self.move(movement)
+        return adjs
+
+    def next_direction(self, movement):
+        if movement == Direction.RIGHT:
+            # upper cel is empty and upper right cel was not filled yet
+            if (self.matrix[self.x + 1, self.y] == 0 and self.matrix[self.x - 1, self.y] == 0):
+                movement = Direction.UP
+        elif movement == Direction.UP:
+            # left cel is empty, move left
+            if (self.matrix[self.x, self.y - 1] == 0):
+                movement = Direction.LEFT
+        elif movement == Direction.LEFT:
+            # lower cel is empty, move down
+            if (self.matrix[self.x + 1, self.y] == 0):
+                movement = Direction.DOWN
+        elif movement == Direction.DOWN:
+            # right cel is empty, move right
+            if (self.matrix[self.x, self.y + 1] == 0):
+                movement = Direction.RIGHT
+        return movement
 
     def move(self, direction):
         if direction == Direction.RIGHT:
@@ -28,57 +78,47 @@ class Spiral:
         elif direction == Direction.DOWN:
             self.x += 1
 
-        if self.x >= len(self.matrix) or self.y >= len(self.matrix):
+        if self.x+1 >= len(self.matrix) or self.y+1 >= len(self.matrix):
             raise Exception('Not enough space!')
 
+    def assign(self, value):
+        self.matrix[self.x, self.y] = value
+            
 
-    def fill(self, max):
-        movement = Direction.DOWN
+    def adjacents(self):
+        adjs = []
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                adjs.append(self.matrix[self.x + x, self.y + y])
+        return sum(adjs) if sum(adjs) > 0 else 1
 
-        while self.current < max:
-            if movement == Direction.RIGHT:
-                # upper cel is empty and upper right cel was not filled yet
-                if (self.matrix[self.x + 1, self.y] == 0 and self.matrix[self.x - 1, self.y] == 0):
-                    movement = Direction.UP
-            elif movement == Direction.UP:
-                # left cel is empty, move left
-                if (self.matrix[self.x, self.y - 1] == 0):
-                    movement = Direction.LEFT
-            elif movement == Direction.LEFT:
-                # lower cel is empty, move down
-                if (self.matrix[self.x + 1, self.y] == 0):
-                    movement = Direction.DOWN
-            elif movement == Direction.DOWN:
-                # right cel is empty, move right
-                if (self.matrix[self.x, self.y + 1] == 0):
-                    movement = Direction.RIGHT
-                
-            self.assign()
-            self.move(movement)
+    def manhattan_distance(self, data):
+        start = self.starting_point()
+        position = self.find(data)
+        distance = [abs(start[0] - position[0]), abs(start[1] - position[1])]
+        return sum(distance)
 
-def starting_point(matrix):
-    center = len(matrix) / 2
-    return center, center
+    def find(self, data):
+        size = len(self.matrix)
 
-def find(matrix, data):
-    size = len(matrix) - 1
+        for x in range(0, size):
+            for y in range(0, size):
+                if self.matrix[x,y] == data:
+                    return [x, y]
+        
+        raise Exception('Item was not found!')
 
-    for x in range(0, size):
-        for y in range(0, size):
-            if matrix[x,y] == data:
-                return [x, y]
-    
-    raise Exception('Item was not found!')
 
-def manhattan_distance(matrix, data):
-    start = starting_point(matrix)
-    position = find(matrix, data)
 
-    distance = [abs(start[0] - position[0]), abs(start[1] - position[1])]
-    return sum(distance)
+
 
 input = 265149
 
-s = Spiral()
-s.fill(input)
-print manhattan_distance(s.matrix, input)
+if True:
+    s = Spiral(600)
+    s.fill(input)
+    print s.manhattan_distance(input)
+
+if True:
+    s = Spiral(600)
+    print s.stress_test(265149)
