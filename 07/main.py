73232fd53da475e682bca3b841ba8940b9c16458
Thiago Bohn <thiago.bohn@sap.com>
2017-12-09 07:55:53 -0200

    Day 7 Second Star

--- 07/main.py
+++ 07/main.py
@@ -3,8 +3,6 @@ import re
 data = list(open('07\input.txt').readlines())
 data = map(lambda l: l.split('->'), data)
 
-print data
-
 class Tower():
 
     def __init__(self, name):
@@ -16,6 +14,15 @@ class Tower():
     def add_child(self, child):
         self.childs[child.name] = child
         child.parent = self
+
+    def get_weight(self):
+        if len(self.childs) == 0:
+            return self.weight
+        else:
+            total_weight = 0
+            for child in self.childs:
+                total_weight += self.childs[child].get_weight()
+            return total_weight  + self.weight
 
 class TowersList():
     def __init__(self):
@@ -50,6 +57,28 @@ class TowersList():
             if not tower.parent:
                 return tower
 
+def find_unbalaced_tower(tower, level):
+    for t in tower.childs:
+        child = tower.childs[t]
+        print " " * 2 * level + child.name + " " + str(child.get_weight()) + " " + str(child.weight)
+        
+        for s in child.childs:
+            continue
+            find_unbalaced_tower(child.childs[s], level + 1)
+
 lst = TowersList()
 lst.process(data)
-print lst.get_main_tower().name
+main = lst.get_main_tower()
+
+print main.name + " " + str(main.get_weight()) + " " + str(main.weight)
+
+find_unbalaced_tower(lst.get_tower('hlqnsbe'), 1)
+print '----------'
+find_unbalaced_tower(lst.get_tower('rilyl'), 2)
+print '----------'
+find_unbalaced_tower(lst.get_tower('aurik'), 3)
+print '----------'
+find_unbalaced_tower(lst.get_tower('jriph'), 4)
+
+# done via debug
+lst.get_tower('jriph').weight = 1993
