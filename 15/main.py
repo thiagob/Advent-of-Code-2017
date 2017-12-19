8c872288abc3c5bf4f424c808aba0a810f387071
Thiago Bohn <thiago.bohn@sap.com>
2017-12-18 23:28:38 -0200

    Day 15 - Second Part

--- 15/main.py
+++ 15/main.py
@@ -1,24 +1,27 @@
 class Generator():
 
-    def __init__(self, start, factor):
+    def __init__(self, start, factor, multiple_of=1):
         self.previous = start
         self.factor = factor
         self.divisor = 2147483647
+        self.multiple_of = multiple_of
 
     
     def get_next(self):
-        next = (self.previous * self.factor) % self.divisor
-        self.previous = next
-        return next
+        while True:
+            next = (self.previous * self.factor) % self.divisor
+            self.previous = next
+
+            if next % self.multiple_of == 0:
+                return next
 
 class Judge():
 
-    def __init__(self, gen_a, gen_b):
+    def __init__(self, gen_a, gen_b, num_of_pairs=40000000):
         self.gen_a = gen_a
         self.gen_b = gen_b
         self.count = 0
-        self.num_of_pairs = 40000000
-        #self.num_of_pairs = 5
+        self.num_of_pairs = num_of_pairs
 
     def get_lowest_bits(self, num):
         return self.get_binary(num)[16:]
@@ -39,13 +42,29 @@ class Judge():
             if progress % 5 == 0:
                 print '{0}% ==> Count: {1}'.format(progress, self.count)
 
-#ga = Generator(65, 16807)
-#gb = Generator(8921, 48271)
+# sample
+# ga = Generator(65, 16807)
+# gb = Generator(8921, 48271)
 
-ga = Generator(618, 16807)
-gb = Generator(814, 48271)
+# input
+# ga = Generator(618, 16807)
+# gb = Generator(814, 48271)
 
-jdg = Judge(ga, gb)
+# jdg = Judge(ga, gb)
+# jdg.run()
+
+# print '!!!!! Part1 >> Final Count: {0} !!!!!'.format(jdg.count)
+
+
+# sample
+# ga = Generator(65, 16807, 4)
+# gb = Generator(8921, 48271, 8)
+
+# input
+ga = Generator(618, 16807, 4)
+gb = Generator(814, 48271, 8)
+
+jdg = Judge(ga, gb, 5000000)
 jdg.run()
 
-print '!!!!! Final Count: {0} !!!!!'.format(jdg.count)
+print '!!!!! Part2   >> Final Count: {0} !!!!!'.format(jdg.count)
