ecd81eb6fa57e1ccc4d7f279f10e2c1e663b0b2b
Thiago Bohn <thiago.bohn@sap.com>
2017-12-29 16:40:26 -0200

    Day 20 - Second Part

--- 20/main.py
+++ 20/main.py
@@ -32,4 +32,63 @@ def find_lower_acceleration(particles):
 
     return acc.index(min(acc))
 
+def sort_by_position(particle):
+    return particle["position"]["x"], particle["position"]["y"], particle["position"]["z"]
+
+class Simulator():
+
+    def __init__(self, particles):
+        self.particles = particles[:]
+        self.time = 0
+
+    def simulate(self):
+        self.export()
+
+        for i in range(0, 10000):
+            self.move()
+            self.find_collisions()
+            self.export()
+
+    def move(self):
+        self.time += 1
+        for p in self.particles:
+            for dimension in ['x', 'y', 'z']:
+                p["velocity"][dimension] += p["acceleration"][dimension]
+                p["position"][dimension] += p["velocity"][dimension]
+
+    def find_collisions(self):
+        self.particles = sorted(self.particles, key=sort_by_position)
+        collisions = []
+
+        index = 0
+        while index < len(self.particles):
+
+            inc = 1
+            while index + inc < len(self.particles) and \
+                    self.is_collision(self.particles[index], self.particles[index + inc]):
+                if not index in collisions:
+                    collisions.append(index)
+                collisions.append(index + inc)
+                inc += 1
+
+            index += inc
+
+        for index in reversed(collisions):
+            del self.particles[index]
+
+    def is_collision(self, p1, p2):
+        return p1["position"]["x"] == p2["position"]["x"] and \
+            p1["position"]["y"] == p2["position"]["y"] and \
+            p1["position"]["z"] == p2["position"]["z"]
+             
+
+    def export(self):
+        space = []
+        for p in self.particles:
+            space.append([p["position"]["x"], p["position"]["y"], p["position"]["z"]]) 
+        print '{0} particles >> {1}'.format(len(self.particles), space if len(space) < 10 else 'Too big' ) 
+
 print find_lower_acceleration(particles)
+
+s = Simulator(particles)
+s.simulate()
